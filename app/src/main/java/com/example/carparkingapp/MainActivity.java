077f036c0a7f89e52568d7c560897ae78f7dbbab
Aisha-code1 <techaishas@gmail.com>
2025-06-19 12:34:42 +0500

    add profile activity

--- app/src/main/java/com/example/carparkingapp/MainActivity.java
+++ app/src/main/java/com/example/carparkingapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(() -> {
-            Intent intent  = new Intent(MainActivity.this, RegisterActivity.class);
+            Intent intent  = new Intent(MainActivity.this, LoginandRegisterActivity.class);
             startActivity(intent);
             finish();
 
