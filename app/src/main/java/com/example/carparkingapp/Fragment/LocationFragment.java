133ccf0197d5a2ab685074285b0a9bcd9ae54462
Aisha-code1 <techaishas@gmail.com>
2025-08-06 14:37:02 +0500

    map updated

--- app/src/main/java/com/example/carparkingapp/Fragment/LocationFragment.java
+++ app/src/main/java/com/example/carparkingapp/Fragment/LocationFragment.java
@@ -1,18 +1,23 @@
 package com.example.carparkingapp.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.carparkingapp.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,56 +27,32 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class LocationFragment extends Fragment implements OnMapReadyCallback {
-    private GoogleMap mMap;
-    private FusedLocationProviderClient fusedlocationclient;
 
-    public LocationFragment() {
-        // Required empty public constructor
-    }
-    private final LatLng[] malllocations = {
-            new LatLng(32.1618, 74.1883), // King's Mall
-            new LatLng(32.1829, 74.2042), // Mall of Gujranwala
-            new LatLng(32.1656, 74.1875)
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private final LatLng[] mallLocations = {
+            new LatLng(32.102735, 74.189499), // King's Mall
+            new LatLng(32.181284, 74.182274), // Chase Up Shopping Center
+            new LatLng(32.167565, 74.197368)  // Rainbow Mall
     };
+
     private final String[] mallNames = {
             "King's Mall",
-            "Mall of Gujranwala",
-            "G-Town Mall"
+            "Chase Up Shopping Center",
+            "Rainbow Mall"
     };
 
-    private final String[] mallMapLinks = {
-            "https://maps.google.com/?q=32.1618,74.1883",
-            "https://maps.google.com/?q=32.1829,74.2042",
-            "https://maps.google.com/?q=32.1656,74.1875",
-
-    };
-//    public static LocationFragment newInstance(String param1, String param2) {
-//        LocationFragment fragment = new LocationFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-   }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_location, container, false);
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
 
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.map);
@@ -84,11 +65,48 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng gujranwalaCenter = new LatLng(32.157351, 74.184507);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gujranwalaCenter, 12));
+        showMallMarkers();
+        enableMyLocation();
+    }
+
+    private void showMallMarkers() {
+        for (int i = 0; i < mallLocations.length; i++) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(mallLocations[i])
+                    .title(mallNames[i]));
+        }
+    }
 
 
-        // Marker and Camera move
-        LatLng Gujranwala = new LatLng(32.1877, 74.1945);
-        mMap.addMarker(new MarkerOptions().position(Gujranwala).title("Parking spot in Gujranwala"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Gujranwala, 15));
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(location -> {
+                        if (location != null) {
+                            LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions()
+                                    .position(userLatLng)
+                                    .title("Your Location"));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 12));
+                        }
+                    });
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1001);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1001) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation();
+            } else {
+                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
